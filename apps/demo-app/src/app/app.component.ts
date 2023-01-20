31563f247e62a5ff0c2ec8dbdcb1c1db0c18106d
Justin <not@rightnow.nl>
2023-01-20 15:00:08 +0100

    Moved demo-app over to standalone and added tw

--- apps/demo-app/src/app/app.component.ts
+++ apps/demo-app/src/app/app.component.ts
@@ -1,9 +1,12 @@
+import { RouterModule } from '@angular/router';
 import { Component } from '@angular/core';
 
 @Component({
+  standalone: true,
+  imports: [RouterModule],
   selector: 'jv-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less'],
+  template: `<h1>hey</h1>`,
+  styles: [],
 })
 export class AppComponent {
   title = 'demo-app';
