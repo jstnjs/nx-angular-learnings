22e22a0066f897a89448cfa4ce27e3b1e7ba432d
Justin <not@rightnow.nl>
2023-01-22 16:43:06 +0100

    Added store

--- libs/demo-app/features/shell/src/lib/shell/shell.component.ts
+++ libs/demo-app/features/shell/src/lib/shell/shell.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { Store } from '@ngrx/store';
+import * as userActions from './+state/users.actions'
 
 @Component({
   selector: 'jv-demo-app-shell',
@@ -8,4 +10,10 @@ import { RouterModule } from '@angular/router';
   imports: [CommonModule, RouterModule],
   template: `<router-outlet />`,
 })
-export class ShellComponent {}
+export class ShellComponent implements OnInit{
+  store = inject(Store)
+  ngOnInit(): void {
+      // this.facade.init();
+      this.store.dispatch(userActions.initUsers());
+  }
+}
--- libs/demo-app/features/shell/src/lib/shell/shell.routes.ts
+++ libs/demo-app/features/shell/src/lib/shell/shell.routes.ts
@@ -1,19 +1,31 @@
 import { Route } from '@angular/router';
 import { LayoutComponent } from '@jv/demo-app/ui/layout';
+import { provideStore, provideState } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import * as fromUsers from './+state/users.reducer';
+import { UsersEffects } from './+state/users.effects';
+import { UsersFacade } from './+state/users.facade';
 
 export const demoAppShellRoutes: Route[] = [
-  { 
-    path: '', 
+  {
+    path: '',
     component: LayoutComponent,
     children: [
       {
         path: '',
-        loadComponent: () => import('@jv/demo-app/features/home').then(c => c.HomeComponent),
+        loadComponent: () =>
+          import('@jv/demo-app/features/home').then((c) => c.HomeComponent),
       },
       {
         path: 'login',
-        loadComponent: () => import('@jv/demo-app/features/login').then(c => c.LoginComponent),
-      }
-    ] 
+        loadComponent: () =>
+          import('@jv/demo-app/features/login').then((c) => c.LoginComponent),
+      },
+    ],
+    providers: [
+      UsersFacade,
+      provideState(fromUsers.USERS_FEATURE_KEY, fromUsers.usersReducer),
+      provideEffects(UsersEffects),
+    ],
   },
 ];
