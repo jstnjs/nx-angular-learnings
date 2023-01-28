4d3fdc3f9137db598e761d0811f14037b2838245
Justin <not@rightnow.nl>
2023-01-28 15:02:51 +0100

    Create protected api request

--- apps/auth-api/src/auth/auth.module.ts
+++ apps/auth-api/src/auth/auth.module.ts
@@ -5,6 +5,7 @@ import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './local.strategy';
 import { JwtModule } from '@nestjs/jwt';
 import { jwtConstants } from './constants';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [
@@ -12,9 +13,9 @@ import { jwtConstants } from './constants';
     PassportModule,
     JwtModule.register({
       secret: jwtConstants.secret,
-      signOptions: { expiresIn: '60s'},
+      signOptions: { expiresIn: '900s'},
     })],
-  providers: [AuthService, LocalStrategy],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
   exports: [AuthService]
 })
 export class AuthModule {}
