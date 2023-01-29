68ae490e29a81116ce303eb4212987f6541cb30c
Justin <not@rightnow.nl>
2023-01-29 16:27:53 +0100

    Testing auth

--- libs/demo-app/features/home/src/lib/home/home.component.ts
+++ libs/demo-app/features/home/src/lib/home/home.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ApiService } from '@jv/shared/core/http-client';
 
 @Component({
   selector: 'jv-demo-app-home',
@@ -8,4 +9,14 @@ import { CommonModule } from '@angular/common';
   templateUrl: './home.component.html',
   styles: [],
 })
-export class HomeComponent {}
+export class HomeComponent {
+
+  apiService = inject(ApiService);
+
+  aTestRequest() {
+    console.log('works?')
+    this.apiService.get('/profile').subscribe((testingg => {
+      console.log(testingg);
+    }))
+  }
+}
