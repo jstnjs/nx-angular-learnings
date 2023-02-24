d99ffcefa661d2eaa4074951107e77cc8573c364
Justin <not@rightnow.nl>
2023-02-24 17:19:37 +0100

    Cleanup

--- libs/shared/data-access/auth/src/lib/auth/+state/auth.effects.ts
+++ libs/shared/data-access/auth/src/lib/auth/+state/auth.effects.ts
@@ -1,24 +1,24 @@
-import { inject, Injectable } from "@angular/core";
-import { createEffect } from "@ngrx/effects";
-import { Actions, ofType } from "@ngrx/effects";
-import { catchError, exhaustMap, map, of } from "rxjs";
-import { AuthService } from "../services/auth.service";
-import { authActions } from "./auth.actions";
+import { inject, Injectable } from '@angular/core';
+import { createEffect } from '@ngrx/effects';
+import { Actions, ofType } from '@ngrx/effects';
+import { catchError, exhaustMap, map, of } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+import { authActions } from './auth.actions';
 
 @Injectable()
 export class AuthEffects {
-    actions$ = inject(Actions);
-    authService = inject(AuthService);
+  actions$ = inject(Actions);
+  authService = inject(AuthService);
 
-    login$ = createEffect(() => 
-        this.actions$.pipe(
-            ofType(authActions.login),
-            exhaustMap(action => 
-                this.authService.login(action).pipe(
-                    map(user => authActions.loginSuccess({user})),
-                    catchError(error => of(authActions.loginFailure({error})))
-                )
-            )
-        )
-    )
+  login$ = createEffect(() => {
+    return this.actions$.pipe(
+      ofType(authActions.login),
+      exhaustMap((action) =>
+        this.authService.login(action).pipe(
+          map((user) => authActions.loginSuccess({ user })),
+          catchError((error) => of(authActions.loginFailure({ error }))),
+        ),
+      ),
+    );
+  });
 }
