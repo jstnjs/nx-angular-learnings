6680fa6e31577be0facbf7a4de905c2e46ac6524
Justin <justin@justins-mbp.kpn>
2023-03-22 19:28:45 +0100

    Add button lib

---
+++ libs/shared/ui/button/src/lib/button/button.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'jv-button',
+  standalone: true,
+  imports: [CommonModule],
+  template: `<button>Test</button>`,
+  styles: [],
+})
+export class ButtonComponent {}
