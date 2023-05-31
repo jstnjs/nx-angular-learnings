ab79ada6cd446640529c35ef63edb612896d0e82
Justin <not@rightnow.nl>
2023-05-31 20:49:55 +0200

    Integrate session storage

--- libs/demo-app/features/experiments/src/lib/experiments/task.service.ts
+++ libs/demo-app/features/experiments/src/lib/experiments/task.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { filter, shareReplay, startWith, tap } from 'rxjs/operators';
 
 @Injectable({
@@ -9,10 +9,19 @@ import { filter, shareReplay, startWith, tap } from 'rxjs/operators';
 export class TaskService {
   private cache$: Observable<any[]> | undefined;
   http = inject(HttpClient);
+  private cacheKey = 'tasksCache';
 
   get tasks(): Observable<any> {
+    const cachedTasks = sessionStorage.getItem(this.cacheKey);
+    if (cachedTasks) {
+      this.cache$ = of(JSON.parse(cachedTasks));
+    }
+
     if (!this.cache$) {
-      this.cache$ = this.requestTasks().pipe(shareReplay(1));
+      this.cache$ = this.requestTasks().pipe(
+        shareReplay(1),
+        tap((response) => sessionStorage.setItem(this.cacheKey, JSON.stringify(response))),
+      );
     }
 
     return this.cache$;
