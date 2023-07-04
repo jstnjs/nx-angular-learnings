7406c29ab7bc8579b6521057a5009e15d2ede82f
Justin <not@rightnow.nl>
2023-07-04 20:19:05 +0200

    Add wizard lib

---
+++ libs/shared/feature/wizard/src/lib/wizard/wizard.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'jv-wizard',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './wizard.component.html',
+  styles: [],
+})
+export class WizardComponent {}
