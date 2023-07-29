d46fbe26a497f8fb2a81a784cb05caf3ee626f13
Justin <not@rightnow.nl>
2023-07-29 16:20:35 +0200

    Projects should only include demo app

--- apps/demo-app/src/app/app.component.ts
+++ apps/demo-app/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { ShellComponent } from '@jv/demo-app/features/shell'
+import { ShellComponent } from '@jv/demo-app/features/shell';
 
 @Component({
   standalone: true,
   imports: [ShellComponent],
   selector: 'jv-root',
-  template: `<jv-demo-app-shell />`,
+  template: `<jv-demo-app-shell /> +++`,
   styles: [],
 })
 export class AppComponent {
